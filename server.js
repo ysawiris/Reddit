acf6b3b5765ba64540e2b2c51e370b4fb17de051
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2020-02-03 19:57:52 -0800

    Users can submit and save posts

--- server.js
+++ server.js
@@ -1,22 +1,41 @@
 // Require Libraries
 const express = require('express');
 
+
 // App Setup
 const app = express();
-
+require('./controllers/posts.js')(app);
 // Middleware
 const exphbs  = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+// Set db
+require('./data/reddit-db');
+
 // Routes
 app.get('/', (req, res) => {
     res.render('layouts/main.handlebars');
   });
+
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new.handlebars')
+})
 
 // Start Server
 
 app.listen(3000, () => {
   console.log('Reddit listening on port localhost:3000!');
 });
+
