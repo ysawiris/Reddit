c53c80bdcfc269adca3886edba4b047c73b6535d
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2020-02-04 20:27:13 -0800

    working on viewing all posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -15,5 +15,15 @@ module.exports = (app) => {
       return res.redirect(`/`);
     })
   });
+  app.get('/', (req, res) => {
+    Post.find({})
+    .then(posts => {
+      res.render("posts-index.handlebars", { posts });
+    })
+    .catch(err => {
+      console.log(err.message);
+    })
+  });
+
 
 };
