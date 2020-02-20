42356b62658907b68e1b4e34d10764539c3c26db
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2020-02-19 20:10:51 -0800

    Users can log in

--- controllers/auth.js
+++ controllers/auth.js
@@ -31,4 +31,40 @@ module.exports = app => {
         res.clearCookie('nToken');
         res.redirect('/');
     });
+
+    // LOGIN FORM
+    app.get('/login', (req, res) => {
+        res.render('login');
+    });
+
+    // LOGIN
+    app.post("/login", (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        // Find this user name
+        User.findOne({ username }, "username password")
+            .then(user => {
+                if (!user) {
+                    // User not found
+                    return res.status(401).send({ message: "Wrong Username or Password" });
+                }
+                // Check the password
+                user.comparePassword(password, (err, isMatch) => {
+                    if (!isMatch) {
+                        // Password does not match
+                        return res.status(401).send({ message: "Wrong Username or password" });
+                    }
+                    // Create a token
+                    const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+                        expiresIn: "60 days"
+                    });
+                    // Set a cookie and redirect to root
+                    res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+                    res.redirect("/");
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
 };
