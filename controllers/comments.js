3e11b62f9e83bed401f05078ff1624d531cfe7ec
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2020-02-25 20:51:06 -0800

    Authors for comments are visible

--- controllers/comments.js
+++ controllers/comments.js
@@ -7,6 +7,7 @@ module.exports = function(app) {
     app.post("/posts/:postId/comments", function(req, res) {
         // INSTANTIATE INSTANCE OF MODEL
         const comment = new Comment(req.body);
+        comment.author = req.user._id;
 
         // SAVE INSTANCE OF Comment MODEL TO DB
         comment
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,46 +1,67 @@
 //connecting our control with our model
 const Post = require('../models/post');
+const User = require('../models/user');
 
 
 module.exports = (app) => {
     // CREATE
     app.post("/posts/new", (req, res) => {
         if (req.user) {
-            const post = new Post(req.body);
+            var post = new Post(req.body);
+            post.author = req.user._id;
 
-            post.save(function(err, post) {
-                return res.redirect(`/`);
-            });
+            post
+                .save()
+                .then(post => {
+                    return User.findById(req.user._id);
+                })
+                .then(user => {
+                    user.posts.unshift(post);
+                    user.save();
+                    // REDIRECT TO THE NEW POST
+                    res.redirect(`/posts/${post._id}`);
+                })
+                .catch(err => {
+                    console.log(err.message);
+                });
         } else {
             return res.status(401); // UNAUTHORIZED
         }
     });
 
-    app.get("/", (req, res) => {
+    // INDEX
+    app.get('/', (req, res) => {
         const currentUser = req.user;
-
-        Post.find({})
+        // res.render('home', {});
+        console.log(req.cookies);
+        Post.find().populate('author')
             .then(posts => {
-                res.render("posts-index", { posts, currentUser });
+                res.render('posts-index', { posts, currentUser });
+                // res.render('home', {});
+            }).catch(err => {
+                console.log(err.message);
+            })
+    })
+
+    // SHOW
+    app.get("/posts/:id", function(req, res) {
+        const currentUser = req.user;
+        // LOOK UP THE POST
+
+        Post.findById(req.params.id).populate({ path: 'comments', populate: { path: 'author' } }).populate('author')
+            .then(post => {
+                res.render("posts-show", { post, currentUser });
             })
             .catch(err => {
                 console.log(err.message);
             });
     });
-
-    app.get("/posts/:id", function(req, res) {
-        // LOOK UP THE POST
-        Post.findById(req.params.id).populate('comments').then((post) => {
-            res.render('posts-show', { post })
-        }).catch((err) => {
-            console.log(err.message)
-        })
-    });
     // SUBREDDIT
     app.get("/n/:subreddit", function(req, res) {
-        Post.find({ subreddit: req.params.subreddit })
+        const currentUser = req.user;
+        Post.find({ subreddit: req.params.subreddit }).populate('author')
             .then(posts => {
-                res.render("posts-index", { posts });
+                res.render("posts-index", { posts, currentUser });
             })
             .catch(err => {
                 console.log(err);
