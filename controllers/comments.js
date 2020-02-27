542d048327d312a9579ba583144cedfbcd4f1109
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2020-02-26 21:51:25 -0800

    add more tests

--- controllers/comments.js
+++ controllers/comments.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
     app.post("/posts/:postId/comments", function(req, res) {
         // INSTANTIATE INSTANCE OF MODEL
         const comment = new Comment(req.body);
-        comment.author = req.user._id;
+        comment.author = req.user;
 
         // SAVE INSTANCE OF Comment MODEL TO DB
         comment
--- test/index.js
+++ test/index.js
@@ -6,19 +6,19 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe("site", function() {
-  // Describe what you are testing
-  it("Should have home page", function(done) {
-    // Describe what should happen
-    // In this case we test that the home page loads
-    chai
-      .request(app)
-      .get("/")
-      .end(function(err, res) {
-        if (err) {
-          return done(err);
-        }
-        res.status.should.be.equal(200);
-        return done(); // Call done if the test completed successfully.
-      });
-  });
+    // Describe what you are testing
+    it("Should have home page", function(done) {
+        // Describe what should happen
+        // In this case we test that the home page loads
+        chai
+            .request(app)
+            .get("/")
+            .end(function(err, res) {
+                if (err) {
+                    return done(err);
+                }
+                res.status.should.be.equal(200);
+                return done(); // Call done if the test completed successfully.
+            });
+    });
 });
